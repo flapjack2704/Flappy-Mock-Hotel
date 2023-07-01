51f9e5e0a72b1092a95b79dc7cecccd966a7dd85
flapjack2704 <104648123+flapjack2704@users.noreply.github.com>
2023-07-01 06:52:44 +0100

    gets and posts in controller

--- Controllers/HotelBookingController.cs
+++ Controllers/HotelBookingController.cs
@@ -1,10 +1,14 @@
 using Flappy_Mock_Hotel.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Flappy_Mock_Hotel.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/booking")]
     public class HotelBookingController : ControllerBase
     {
 
@@ -17,6 +21,101 @@ namespace Flappy_Mock_Hotel.Controllers
             this.context = context;
         }
 
-        
+        [HttpGet]
+        [Route("rooms")]
+        public Task<List<Room>> GetAllRooms()
+        {
+            return context.Rooms
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("rooms")]
+        public async Task<Room> AddNewRoom([FromBody] Room newRoom)
+        {
+            context.Add(newRoom);
+            await context.SaveChangesAsync();
+            return newRoom;
+        }
+
+        [HttpGet]
+        [Route("bookings")]
+        public Task<List<Booking>> GetAllBookings()
+        {
+            return context.Bookings
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("bookings")]
+        public async Task<Booking> AddNewBooking([FromBody] Booking newBooking)
+        {
+            context.Add(newBooking);
+            await context.SaveChangesAsync();
+            return newBooking;
+        }
+
+
+
+        [HttpGet]
+        [Route("guests")]
+        public Task<List<Guest>> GetAllGuests()
+        {     
+            return context.Guests
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("guests")]
+        public async Task<Guest> AddNewGuest([FromBody] Guest newGuest)
+        {
+            context.Add(newGuest);
+            await context.SaveChangesAsync();
+            return newGuest;
+        }
+
+        [HttpGet]
+        [Route("guests-with-billings")]
+        public async Task<List<Guest>> GetAllGuestsWithBillings()
+        {
+            foreach (var guest in context.Guests.ToList())
+            {
+                await context.Entry(guest).Collection(g => g.Billings).LoadAsync();
+            }
+
+            return context.Guests.ToList();
+        }
+
+        [HttpGet]
+        [Route("guests-with-bookings")]
+        public async Task<List<Guest>> GetAllGuestsWithBookings()
+        {
+
+            foreach (var guest in context.Guests.ToList())
+            {
+                await context.Entry(guest).Collection(g => g.Bookings).Query().Include(b => b.Room).LoadAsync();
+            }
+
+            return context.Guests.ToList();
+        }
+
+
+
+        [HttpGet]
+        [Route("billings")]
+        public Task<List<Billing>> GetAllBillings()
+        {
+            return context.Billings
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("billings")]
+        public async Task<Billing> AddNewBilling([FromBody] Billing newBilling)
+        {
+            context.Add(newBilling);
+            await context.SaveChangesAsync();
+            return newBilling;
+        }
     }
 }
